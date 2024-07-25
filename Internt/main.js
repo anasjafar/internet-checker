3968689517cd821f9f17cb602b0ab25036211a49
anasjafar <164956593+anasjafar@users.noreply.github.com>
2024-07-25 22:56:02 +0300

    Add files via upload

---
+++ Internt/main.js
@@ -0,0 +1,38 @@
+let title = document.querySelector('.title');
+let ul = document.querySelector('ul');
+let reload = document.querySelector('.reload');
+
+window.onload = function() {
+    if (window.navigator.onLine) {
+        online();
+    } else {
+        offline();
+    }
+};
+
+window.addEventListener("online", function() {
+    online();
+});
+
+window.addEventListener("offline", function() {
+    offline();
+});
+
+
+reload.onclick = function(){
+    window.location.reload();
+}
+
+function online() {
+    title.innerHTML = 'Online Now';
+    title.style.color = 'green';
+    ul.classList.add('hide');
+    reload.classList.add('hide');
+}
+
+function offline() {
+    title.innerHTML = 'Offline Now';
+    title.style.color = '#666';
+    reload.classList.remove('hide');
+    ul.classList.remove('hide');
+}
